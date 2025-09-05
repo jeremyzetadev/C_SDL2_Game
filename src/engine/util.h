a08ea37de6c9a72eabebad8939fa0d6b792e1276
jeremy zeta <jeremy_zeta@yahoo.com>
2025-09-06 00:25:04 +0900

    add Error Return, Error Exit

--- src/engine/util.h
+++ src/engine/util.h
@@ -5,5 +5,9 @@
 
 #define ERROR_EXIT(...) {fprintf(stderr, __VA_ARGS__); exit(1);}
 #define ERROR_RETURN(R, ...) {fprintf(stderr, __VA_ARGS__); return R;}
+//Mike Shah macro error check sample
+//#define GLCheck(x) GLClearAllErrors(); x; GLCheckErrorStatus();
+//use GLCheck to a function
+//GLCheck(glBindBuffer(GL_ARRAY_BUFFER, gVertexBufferObject));
 
 #endif
